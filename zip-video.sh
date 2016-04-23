eadee21c4b1242f37cbfc92aa502a6be2325eb6a
onepas <minhnv@fscvietnam.com>
2016-04-23 16:18:30 +0700

    fixed update addons

--- zip-video.sh
+++ zip-video.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 VERSION="$1"
-FILE="plugin.video.vietmedia.movie-$VERSION.zip"
-mv plugin.video.vietmedia.movie/*.zip* ./
-rm plugin.video.vietmedia.movie/.DS*
-rm plugin.video.vietmedia.movie/simplejson/.DS*
-rm plugin.video.vietmedia.movie/resources/.DS*
-zip -9 -r $FILE ./plugin.video.vietmedia.movie
+FILE="plugin.video.vietmediaplay-$VERSION.zip"
+mv plugin.video.vietmediaplay/*.zip* ./
+rm plugin.video.vietmediaplay/.DS*
+rm plugin.video.vietmediaplay/simplejson/.DS*
+rm plugin.video.vietmediaplay/resources/.DS*
+zip -9 -r $FILE ./plugin.video.vietmediaplay
 
 md5 -r addons.xml | cut -c 1-32 > addons.xml.md5
 md5 -r $FILE | cut -c 1-32 > $FILE.md5
 
-mv ./plugin.video*.zip* ./plugin.video.vietmedia.movie
+mv ./plugin.video*.zip* ./plugin.video.vietmediaplay
 
 echo $FILE
